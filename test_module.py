ad8ea9198b0c81c9e1cef59219870d6604c76c1d
Evgenii Snezhkov <evgeni./home/puma/Dev/venv/bin/python -m pip install -U mypysnezhkov@ya.ru>
2022-12-24 13:25:09 +0300

    Provide OOP view

--- test_module.py
+++ test_module.py
@@ -1,13 +1,7 @@
-import os
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
-
-files_list = ['main.py', 'readme.md']
-
-
-def test_program():
-    for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+import webbrowser 
+  
+# Take query from user 
+query = input("Search Google: ") 
+  
+# Open the browser and search 
+webbrowser.open_new_tab('http://www.google.com/search?btnG=1&q=%s' % query)
